e227511434052129d95d1acfefdfe0673403ac6f
snbl1993 <maxjanschnabel@googlemail.com>
2024-10-07 20:21:13 +0200

    scan for devices

--- getSensorMac.py
+++ getSensorMac.py
@@ -1,4 +1,12 @@
-from btlewrap.pygatt import PygattBackend
+from pygatt.backends import GATTToolBackend
 import pygatt
 
-pygatt.lescan()
+adapter = GATTToolBackend()
+
+adapter.start()
+
+sensors = adapter.scan(timeout=15)
+
+
+for device in sensors:
+    print(f"Device found: {device['address']} ({device['name']})")

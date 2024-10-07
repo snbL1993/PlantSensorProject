6cdebec29d87e48bd0098c62e9cd106936fe4b6b
snbl1993 <maxjanschnabel@googlemail.com>
2024-10-07 22:12:26 +0200

    getsensordata

--- plantSensor.py
+++ plantSensor.py
@@ -1,9 +1,11 @@
 import psycopg2 
-from pygatt.backends import GATTToolBackend
+import miflora
+from miflora.miflora_poller import MiFloraPoller
+from pygatt.backends import GattToolBackend
 import pygatt
 
 def getsensormac():
-    adapter = GATTToolBackend()
+    adapter = GattToolBackend()
 
     adapter.start()
 
@@ -11,11 +13,18 @@ def getsensormac():
     sensors = []
 
     for device in devices:
-        print(device['name'])
         if "Flower care" in str({device['name']}):
             print(f"Device found: {device['address']} ({device['name']})")
             sensors.append({device['address']})
     print(sensors)
+    return sensors
 
+def getsensordata(sensors: list):
+    data = {}
+    for sensormac in sensors:
+        poller = MiFloraPoller(sensormac, GattToolBackend)
+        data.update({sensormac:["this","is","a","test",1234]})
 
-getsensormac()
+sensors = getsensormac()
+
+getsensordata(sensors)

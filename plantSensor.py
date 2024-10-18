e34a180f16d4025ac104b78c254545cca060079a
snbl1993 <maxjanschnabel@googlemail.com>
2024-10-18 23:24:17 +0200

    fixed main function

--- plantSensor.py
+++ plantSensor.py
@@ -186,10 +186,10 @@ def button2():
 if __name__ == '__main__':
     app.run()
 
-#automaticly poll every 30 minutes from full hour
-while True:
-    if is_full_hour():
-        ongoingPolling(1800)
-        break
-    time.sleep(1)
-    
+    #automaticly poll every 30 minutes from full hour
+    while True:
+        if is_full_hour():
+            ongoingPolling(1800)
+            break
+        time.sleep(1)
+        
